045b39f5acc06a4ef122f7de0bee65e3bd7917a2
jbli <jinbangli@gmail.com>
2013-12-26 16:07:22 +0800

    Create nsq_output.go

---
+++ nsq_output.go
@@ -0,0 +1,45 @@
+package examples
+
+import (
+        "fmt"
+        nsq "github.com/bitly/go-nsq"
+        "github.com/mozilla-services/heka/pipeline"
+)
+
+type NsqOutputConfig struct {
+        Address string `toml:"address"`
+}
+
+type NsqOutput struct {
+        conf    *NsqOutputConfig
+        nsqwriter *nsq.Writer
+}
+
+func (no *NsqOutput) ConfigStruct() interface{} {
+        return &NsqOutputConfig{"192.168.1.44:4160"}
+}
+
+func (no *NsqOutput) Init(config interface{}) error {
+        no.conf = config.(*NsqOutputConfig)
+        no.nsqwriter = nsq.NewWriter(no.conf.Address)
+        return nil
+}
+
+func (no *NsqOutput) Run(or pipeline.OutputRunner, h pipeline.PluginHelper) error {
+        var outgoing string
+        for pack := range or.InChan() {
+                err := writer.PublishAsync("test", []byte(pack.Message.GetPayload()),nil)
+                if err != nil{
+                  or.LogError(fmt.Errorf("error in writer.PublishAsync"))
+                }
+                pack.Recycle()
+        }
+
+        return nil
+}
+
+func init() {
+        pipeline.RegisterPlugin("RedisMQOutput", func() interface{} {
+                return new(RedisMQOutput)
+        })
+}
